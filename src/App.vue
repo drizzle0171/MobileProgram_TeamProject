24d7960c298ee93213387fab1d1e27192bf01c81
drizzle0171 <yongsandew@gmail.com>
2022-05-01 22:10:33 +0900

    LoginUI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <div v-if="showLogin" class="login">
-      <input class='id' v-model="email" type="text" placeholder="example@gmail.com">
+      <img src="./assets/todofordays.png" width="120px">
+      <div>
+        <i class="idIcon fas fa-envelope"></i> <input class='id' v-model="email" type="text" placeholder="example@gmail.com">
+      </div>
+      <div>
+        <i class="passwordIcon fas fa-lock"></i>
       <input class='password' v-model="password" type="password" placeholder="Password">
-      <button class = "signup" @click="addUser">Sign Up</button>
-      <button class = "signin" @click="login">Sign In</button>
-      <button class = "googleSignin" @click="googleSignIn">Sign In using Google</button>
+      </div>
+      <span class = "signUp" @click="addUser"> Sign Up </span> 
+      <span class = "signIn" @click="login"> Sign In </span>
+      <span class = "googleSignin" @click="googleSignIn">
+        <img src="./assets/googleLogo.png" width=10px>
+           &nbsp; Sign In using Google</span>
       <!-- <div class="g-signin2" id="google-signin-btn" data-onsuccess="onSignIn"></div> -->
     </div>
     <div v-if="show">
@@ -199,10 +207,14 @@ export default {
   .shadow {
     box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
   }
+  .idIcon{
+    margin-left:10px ;
+    color: #7ca3bb;
+  }
   .id {
   width: 250px;
-  margin-right: 20px;
-  margin-bottom: ;
+  margin-top: 10px;
+  margin-bottom: 10px;
   padding: 0 10px;
   background-color: #f2f2f2;
   border-top:none;
@@ -211,8 +223,69 @@ export default {
   border-bottom: 2px solid #7ca3bb;
   height: 30px;
 }
- id::placeholder{
+ .id::placeholder{
+  height: 30px;
+  line-height: 30px;
+  padding-bottom: 10px;
+}
+
+  .passwordIcon{
+    margin-left:40px;
+    color: #7ca3bb;
+  }
+  .password {
+  width: 250px;
+  margin-right: 30px;
+  margin-left: 5px;
+  margin-bottom: 20px;
+  padding: 0 10px;
+  background-color: #f2f2f2;
+  border-top:none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 2px solid #7ca3bb;
+  height: 30px;
+  
+}
+ .password::placeholder{
   height: 30px;
   line-height: 30px;
 }
+  .signUp{
+      display: inline-block;
+      width: 80px;
+      padding: 4px;
+      margin-right: 10px;
+      margin-left: 70px;
+      margin-bottom: 10px;
+      border-radius: 5px;
+      background-color: #7ca3bb;
+      color: white;
+      font-family: Arial, Helvetica, sans-serif;
+  }
+    .signIn{
+      display: inline-block;
+      width: 80px;
+      padding: 4px;
+      margin-right: 70px;
+      margin-left: 10px;
+      margin-bottom: 10px;
+      border-radius: 5px;
+      background-color: #7ca3bb;
+      color: white;
+      font-family: Arial, Helvetica, sans-serif;
+  }
+  .googleSignin{
+      display: inline-block;
+      width: 190px;
+      padding: 4px;
+      border-radius: 5px;
+      background-color: #7ca3bb;
+      color: white;
+      margin-top: 10px;
+      font-family: Arial, Helvetica, sans-serif;
+  }
+  .login{
+    margin-top: 230px;
+  }
 </style>
