d97b2f91de4d522deb1badabe0ddb962093547a6
drizzle0171 <yongsandew@gmail.com>
2022-05-30 21:10:08 +0900

    check fix #103

--- src/main.js
+++ src/main.js
@@ -10,12 +10,7 @@ Vue.prototype.$http = axios
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAXzSzGbGYTjzC-hj3O1DLZRe5dgbYbRro",
   authDomain: "todofordays.firebaseapp.com",
@@ -26,9 +21,8 @@ const firebaseConfig = {
   measurementId: "G-Z7SGLL14B4"
 };
 
-// Initialize Firebase
 initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
 
 new Vue({  
   router,
